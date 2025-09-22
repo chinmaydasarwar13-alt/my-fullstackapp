d50ad6c388abd42a5f1d7852880bf2a9043ff02e
Chinmay Dasarwar <chinmaydasarwar13@gmail.com>
2025-09-22 21:34:27 +0530

    Test: Making a small change to trigger workflow

--- client/src/app.js
+++ client/src/app.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hellooo from the React App!</h1>
+        <h1>Hello from the React App!</h1>
         <p>This is a sample application for the GitHub Actions assignment.</p>
       </header>
     </div>
